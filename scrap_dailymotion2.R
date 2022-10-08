c680476cbed0e01e91f208d800c75395ca39c929
pankaz712 <pankaj.negi68@gmail.com>
2022-10-08 20:13:16 +0530

    init commit

---
+++ scrap_dailymotion2.R
@@ -0,0 +1,97 @@
+###### wirting the full program
+
+## first scrap the main index
+
+main.idx = "https://www.dailymotion.com/archived/index.html"
+wbpg.tmp = read_html(main_url)
+urls.level.zero = html_nodes(wbpg.tmp, '.foreground2') %>% html_attr('href')
+nc = grepl(pattern = '2014|2015',urls.level.zero)
+urls.level.zero = urls.level.zero[which(nc==T)]
+
+
+head(urls.level.zero)
+tail(urls.level.zero)
+
+# outer loop
+mydf = data.frame()
+
+for(i in 1:length(urls.level.zero)){
+  closeAllConnections()
+  url.tmp = paste0("https://www.dailymotion.com",urls.level.zero[i])
+  print(url.tmp)
+  #wbpg.tmp = read_html(url.tmp)
+  wbpg.tmp = tryCatch(
+    
+    read_html(url.tmp),
+    error = function(e) e
+    
+  )
+  
+  if(inherits(wbpg.tmp,"error")){
+    next
+  }
+  urls.level.one = html_nodes(wbpg.tmp, '.foreground2') %>% html_attr('href')
+  vc = which(grepl(pattern = "Noon|Morning|Evening",x = urls.level.one)==TRUE)
+  urls.level.one = urls.level.one[vc]
+  
+  for(j in 1:length(urls.level.one)){
+    closeAllConnections()
+    url.tmp = paste0("https://www.dailymotion.com",urls.level.one[j])
+    print(url.tmp)
+    #wbpg.tmp = read_html(url.tmp)
+    wbpg.tmp = tryCatch(
+      
+      read_html(url.tmp),
+      error = function(e) e
+      
+    )
+    
+    if(inherits(wbpg.tmp,"error")){
+      next
+    }
+    urls.level.two = html_nodes(wbpg.tmp, '.label') %>% html_attr('href')
+    urls.level.two =  urls.level.two[-is.na(urls.level.two)]
+    urls.level.two = paste0("https://www.dailymotion.com",urls.level.two)
+    m1 = multi_get(service.urls = urls.level.two)
+    
+    for(k in 1:length(m1)){
+      
+      wbpg.tmp = tryCatch(
+        
+        read_html(unlist(m1[k])),
+        error = function(e) e
+    
+      )
+      
+      if(inherits(wbpg.tmp,"error")){
+        next
+      }
+      
+      heading = html_nodes(wbpg.tmp, '.video_title')
+      text = html_text(heading)
+      urls = html_nodes(wbpg.tmp, '.video_title') %>% html_attr('href')
+      tmp = data.frame(title = text,urls = urls)
+      mydf = rbind(mydf,tmp)
+      #Sys.sleep(time = 1)
+      
+    }
+  
+    Sys.sleep(time = 1)
+    
+  }
+}
+
+
+#  "https://www.dailymotion.com/archived/2014/07/21/Morning/index.html"
+
+
+###
+head(mydf)
+tail(mydf)
+
+###################################
+
+write.csv(x = mydf,file = "mydf.csv")
+
+
+
